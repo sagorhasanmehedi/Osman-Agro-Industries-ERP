42d01419ef8033a97a1bac57fa47724ac3ccca2b
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2022-08-24 18:30:49 +0600

    bank acount details update

--- src/components/bank/bankdeposit/AccountDetails.js
+++ src/components/bank/bankdeposit/AccountDetails.js
@@ -9,28 +9,45 @@ import { Row, Col } from "react-bootstrap";
 const AccountDetails = () => {
   return (
     <>
-      <CardContainer Width="45%">
-        <img
-          src="https://images.indianexpress.com/2022/07/Nitish-kumar-8.jpeg"
-          alt=""
-          style={{
-            width: "80px",
-            height: "80px",
-            borderRadius: "50%",
-          }}
-        />
-        <br />
-        <Lavel>A/c Id:</Lavel>
-        <Input type="number" name="Privious Balance" />
-        <Lavel>A/c Name:</Lavel>
-        <Input type="number" name="Blance Hold" />
-        <Lavel>A/c No:</Lavel>
-        <Input type="text" name="Today Transaction No" />
-        <Lavel>Bank Name:</Lavel>
-        <Input type="text" name="Billing No" />
-        <Lavel>Branch Name:</Lavel>
-        <Input type="text" name="Billing No" />
-      </CardContainer>
+      <Row xs={1} sm={2} style={{ maxWidth: "1091px" }}>
+        <Col>
+          <CardContainer>
+            <Lavel>A/c Id:</Lavel>
+            <Input type="number" name="Privious Balance" />
+            <Lavel>A/c Name:</Lavel>
+            <Input type="number" name="Blance Hold" />
+            <Lavel>A/c No:</Lavel>
+            <Input type="text" name="Today Transaction No" />
+            <Lavel>Bank Name:</Lavel>
+            <Input type="text" name="Billing No" />
+            <Lavel>Branch Name:</Lavel>
+            <Input type="text" name="Billing No" />
+          </CardContainer>
+        </Col>
+        <Col>
+          <CardContainer>
+            <img
+              src="https://images.indianexpress.com/2022/07/Nitish-kumar-8.jpeg"
+              alt="imae"
+              style={{
+                width: "80px",
+                height: "80px",
+                borderRadius: "50%",
+              }}
+            />
+            <br />
+            <Lavel>Previous Balance</Lavel>
+            <Input type="number" name="Privious Balance" />
+            <Lavel>Balance Hold</Lavel>
+            <Input type="number" name="Blance Hold" />
+            <Lavel>Today Transaction No</Lavel>
+            <Input type="text" name="Today Transaction No" />
+
+            <Lavel>Account Status:</Lavel>
+            <Input type="text" name="Email" />
+          </CardContainer>
+        </Col>
+      </Row>
     </>
   );
 };
